6194d161bb5c757bf312faaf410818f7b4bc2ef2
NashPL <nash25pl@gmail.com>
2018-12-15 22:34:06 +0000

    Added more into gitignore, fixed app.js with bug added more lib to detect path

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const wikipedia = require('./../lib/WikipediaAPI');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/wikipedia', (req, res, next) => {
+  res.sendFile(path.resolve('views/index_wikipediaAPI.html'));
+});
+
+router.get('/wikipedia/term/:term', (req, res, next) => {
+  
 });
 
 module.exports = router;
